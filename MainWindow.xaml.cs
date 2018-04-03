b329ba29934337303dd97227db09febb75ed7e61
Borisis <ibentechi@gmail.com>
2018-04-03 17:00:34 +0300

    Многослойный перцептрон

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,7 +24,14 @@ namespace Neural_Application
     public partial class MainWindow : Window
     {
         private UCanWrapper canWrapper;
-        private CNeuron cNeuron;
+        private CNeuron[] cNeuron;
+        private CHidden[] hiddenNeurons;
+
+        private int[] inputData = new int[1024];
+
+        private const int HIDDEN_COUNT = 64;
+        private const int INPUT_COUNT = 1024;
+
 
         public MainWindow()
         {
@@ -35,8 +42,24 @@ namespace Neural_Application
             canWrapper = new UCanWrapper(0, UCanWrapper.eUcanBaudrate.USBCAN_BAUD_500kBit);
             canWrapper.onBDGPDataRcv += DrawGraph;
 
-            cNeuron = new CNeuron();
-            double temp1 = cNeuron.Prov(1.235);
+            hiddenNeurons = new CHidden[HIDDEN_COUNT];
+            for (int i = 0; i < HIDDEN_COUNT; i++)
+                hiddenNeurons[i] = new CHidden();
+
+            /*hiddenNeurons[0].uInputWeight[0] = 0.8f;
+            hiddenNeurons[0].uInputWeight[1] = 0.2f;
+
+            
+            hiddenNeurons[1].uInputWeight[0] = 0.4f;
+            hiddenNeurons[1].uInputWeight[1] = 0.9f;
+            
+            hiddenNeurons[2].uInputWeight[0] = 0.3f;
+            hiddenNeurons[2].uInputWeight[1] = 0.5f;
+
+
+            hiddenNeurons[0].uOutputWeight = 0.3f;
+            hiddenNeurons[1].uOutputWeight = 0.5f;
+            hiddenNeurons[2].uOutputWeight = 0.9f;*/
 
         }
         public void UpdateLogControl()
@@ -60,6 +83,8 @@ namespace Neural_Application
         }
         public void DrawGraph(int[] dataArr)
         {
+            
+
             Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(delegate()
             {
 
@@ -102,9 +127,167 @@ namespace Neural_Application
                 Index++;
 
             }
+            inputData = dataArr;
+            
+            //AnalyzeProcess(dataArr);
+            
+            
             }));
         }
 
+        public void DrawGraph2(float[] dataArr)
+        {
+            
+
+            Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(delegate()
+            {
+
+                float Max = dataArr.Max();
+                int WndY = Convert.ToInt16(myCanvas2.ActualHeight);
+
+                float koefY = 0;
+
+                if (Max > 0)
+                    koefY = WndY / (float)Max;
+                else
+                    koefY = WndY;
+
+
+                myCanvas2.Children.Clear();
+
+                int oldX = 0, oldY = WndY;
+                int Index = 0;
+
+                for (int i = 0; i < dataArr.Length - 1; i += 2)
+                {
+
+                    Line myLine = new Line();
+                    myLine.Stroke = System.Windows.Media.Brushes.Red;
+
+                    myLine.X1 = oldX;
+                    myLine.X2 = i;
+                    myLine.Y1 = oldY;
+
+                    int currentY = (int)(WndY - ((dataArr[i] + dataArr[i + 1]) * 0.5 * koefY));
+
+                    myLine.Y2 = currentY;
+                    // myLine.Y2 = WndY - (dataArr[i] * koefY);
+
+                    oldX = (int)myLine.X2;
+                    oldY = (int)myLine.Y2;
+
+                    myCanvas2.Children.Add(myLine);
+
+                    Index++;
+
+                }
+
+
+            }));
+        }
+
+        public void AnalyzeProcess(int[] dataArr)
+        {
+            float fResult = 0;
+            float fHiddenSum = 0;
+
+            for (int i = 0; i < HIDDEN_COUNT; i++)
+            {
+                hiddenNeurons[i].UpdateInputData(CFunctionsRelease.Normalize(dataArr));
+                hiddenNeurons[i].HiddenProcess();
+
+                fHiddenSum += hiddenNeurons[i].HiddenResult();
+            }
+
+            fResult = CFunctionsRelease.Sigmoid(fHiddenSum);//calculated value
+
+            resLabel.Content = "Result " + fResult.ToString();
+
+            if (backpropChkBx.IsChecked == true)
+            {
+                 BackPropagation(0.0f, fHiddenSum, fResult);
+            }
+        }
+
+        public void BackPropagation(float fTarget,float fHiddenSum,float fCalc)
+        {
+            float[] fDeltaHiddenSum = new float[HIDDEN_COUNT];
+            float[] fNewOutputWeight = new float[HIDDEN_COUNT];
+            float[] fNewInputWeight = new float[INPUT_COUNT];
+
+            float fDeltaOutSum = CFunctionsRelease.Derivate(fHiddenSum) * (fTarget - fCalc);
+
+            debugLabel.Content = "";
+            debugLabel.Content += fDeltaOutSum.ToString() + Environment.NewLine;
+            debugLabel.Content += (fHiddenSum).ToString();
+
+            for(int i = 0;i<HIDDEN_COUNT;i++)
+            {
+                float fNewOutWeight = fDeltaOutSum / hiddenNeurons[i].HiddenSigmoid();
+
+                fNewOutputWeight[i] = hiddenNeurons[i].uOutputWeight + fNewOutWeight;
+            }
+
+            for(int i = 0;i<HIDDEN_COUNT;i++)
+            {
+                fDeltaHiddenSum[i] = fDeltaOutSum / hiddenNeurons[i].uOutputWeight * CFunctionsRelease.Derivate(hiddenNeurons[i].HiddenSum());
+            }
+
+            int iBackInd = HIDDEN_COUNT - 1;
+
+            for (int i = 0; i < HIDDEN_COUNT; i++)
+            {
+                for (int j = 0; j < INPUT_COUNT; j++)
+                {
+                    fNewInputWeight[j] = 0;
+                    if (hiddenNeurons[i].uInputData[j] != 0)
+
+                    fNewInputWeight[j] = hiddenNeurons[i].uInputWeight[j] + fDeltaHiddenSum[i] / hiddenNeurons[i].uInputData[j];
+                    hiddenNeurons[i].uInputWeight[j] = fNewInputWeight[j];
+                }
+               // hiddenNeurons[i].uInputWeight = fNewInputWeight;
+                hiddenNeurons[i].uOutputWeight = fNewOutputWeight[i];
+
+                iBackInd--;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (cyclChkBox.IsChecked == true)
+            {
+                int uCycles = 10000;
+                while (uCycles-- > 0)
+                    AnalyzeProcess(inputData);
+            }
+            else
+            {
+                AnalyzeProcess(inputData);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < INPUT_COUNT; i++)
+                inputData[i] = 0;
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            DrawGraph2(hiddenNeurons[0].uInputWeight);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < INPUT_COUNT; i++)
+                inputData[i] = 1;
+        }
 
     }
 }
--- Neuron/Network.cs
+++ Neuron/Network.cs
@@ -6,32 +6,154 @@ using System.Text;
 
 namespace Neural_Application.Neuron
 {
-    class CNeuron
+    static class CFunctionsRelease
     {
-        const int neuralDataSize = 1024;
-
-        private float[] uSignalData;
-        private float[] uWeightData;
-
-        public CNeuron()
+        static public float Derivate(float x1)
         {
-            uSignalData = new float[neuralDataSize];
-            uWeightData = new float[neuralDataSize];
-
-
-        }
-
-        public float NeuronGetMux(int uIndex)
-        {
-            return uSignalData[uIndex] * uWeightData[uIndex];
-        }
-
-        public double Prov(double x1)
-        {
-            double fResult =  Math.Exp(-x1) / ((1 + Math.Exp(-x1)) * (1 + Math.Exp(-x1))); 
+            float fResult = Convert.ToSingle(Math.Exp(-x1) / ((1 + Math.Exp(-x1)) * (1 + Math.Exp(-x1)))); 
 
             return fResult;
         }
-        
+        static public float Sigmoid(float x1)
+        {
+            float fResult = Convert.ToSingle( 1 / (1 + Math.Exp(-x1)));
+
+            return fResult;
+        }
+        static public float[] Normalize(int[] inputData)
+        {
+            float[] fOutpudData = new float[inputData.Length];
+            int uMax = inputData.Max();
+
+            if (uMax == 0) uMax = 1;
+
+            float fNormKoef = 1.0f / uMax;
+
+            for (int i = 0; i < inputData.Length; i++)
+            {
+                fOutpudData[i] = Convert.ToSingle(inputData[i] * fNormKoef); ;
+            }
+            return fOutpudData;
+        }
+        static public  double SampleGaussian(Random random, double mean, double stddev)
+        {
+            // The method requires sampling from a uniform random of (0,1]
+            // but Random.NextDouble() returns a sample of [0,1).
+            double x1 = 1 - random.NextDouble();
+            double x2 = 1 - random.NextDouble();
+
+            double y1 = Math.Sqrt(-2.0 * Math.Log(x1)) * Math.Cos(2.0 * Math.PI * x2);
+            return y1 * stddev + mean;
+        }
+    }
+
+    class CNeuron
+    {
+
+        const int HIDDEN_SIZE = 3;
+
+        public float fSignal;
+
+        private float[] uSignalWeight;
+
+
+        public CNeuron()
+        {
+            fSignal = 0;
+
+            uSignalWeight = new float[HIDDEN_SIZE];
+        }
+
+
+        public void NeuralProcess(float fData)
+        {
+           
+        }
+
+        public float Derivate(float x1)
+        {
+            float fResult = 1 * Convert.ToSingle(Math.Exp(- 1 * x1) / ((1.0f + Math.Exp(-1 * x1)) * (1.0f + Math.Exp(-1 * x1)))); 
+
+            return fResult;
+        }
+        public float Sigmoid(float x1)
+        {
+            float fResult = Convert.ToSingle( 1.0f / (1.0f + Math.Exp(- 1 * x1)));
+
+            return fResult;
+        }
+    }
+
+    public class CHidden
+    {
+        const int HIDDEN_INPUT_SIZE = 1024;
+
+        public float[] uInputData;
+        public float[] uInputWeight;
+
+        private float uHiddenSum;
+        private float uHiddenData;
+        public float uOutputWeight;
+        private float fHiddenResult;
+
+        private Random wRnd;
+
+        public CHidden()
+        {
+            wRnd = new Random();
+
+            uInputData = new float[HIDDEN_INPUT_SIZE];
+            uInputWeight = new float[HIDDEN_INPUT_SIZE];
+
+            uHiddenData = 0;
+            uHiddenSum = 0;
+            fHiddenResult = 0;
+
+            for (int i = 0; i < HIDDEN_INPUT_SIZE; i++)
+            {
+                uInputWeight[i] = 0.1f;// Convert.ToSingle(CFunctionsRelease.SampleGaussian(wRnd, 0.1, 0.1));
+            }
+            uOutputWeight = 0.2f;// Convert.ToSingle(CFunctionsRelease.SampleGaussian(wRnd, 0.1, 0.1));
+        }
+
+        public void UpdateInputData(float[] fInputData)
+        {
+            for(int i = 0;i<HIDDEN_INPUT_SIZE;i++)
+            {
+                uInputData[i] = fInputData[i];
+            }
+           
+        }
+
+        public void HiddenProcess()
+        {
+           
+            uHiddenData = 0;
+            uHiddenSum = 0;
+
+            for(int i = 0;i<HIDDEN_INPUT_SIZE;i++){
+                uHiddenSum += uInputData[i] * uInputWeight[i];
+            }
+            uHiddenData = CFunctionsRelease.Sigmoid(uHiddenSum);
+
+          
+        }
+
+        public float HiddenResult()
+        {
+            fHiddenResult = uHiddenData * uOutputWeight;
+
+            return fHiddenResult;
+        }
+
+        public float HiddenSigmoid()
+        {
+            return uHiddenData;
+        }
+        public float HiddenSum()
+        {
+            return uHiddenSum;
+        }
+
     }
 }
